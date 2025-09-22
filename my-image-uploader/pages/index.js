882da468220f3134af4df457a7d07eceef421a02
Karthik Vishal <karthikvishal1506@gmail.com>
2025-09-22 21:51:46 +0530

    Added pst metho and configured cloudinary unsigned preset

--- my-image-uploader/pages/index.js
+++ my-image-uploader/pages/index.js
@@ -32,7 +32,24 @@ export default function Home() {
     const form = event.currentTarget
     const fileInput = Array.from(form.elements).find(({ name })=>name === 'file')
     console.log(fileInput)
-    //console.log(event.currentTarget)
+    console.log(event.currentTarget)
+
+    const formData = new FormData();
+    
+    for (const file of fileInput.files){
+      formData.append('file',file)
+    }
+
+    formData.append('upload_preset','my-uploads')
+
+
+
+    const data = await fetch('https://api.cloudinary.com/v1_1/dz2x5ge2n/image/upload',{
+      method: 'POST',
+      body: formData
+    }).then(r=>r.json())
+
+    console.log('data',data)
   }
 
   return (
